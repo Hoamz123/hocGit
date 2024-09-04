4a070d077ef38b9d84fb4d83bf5ac01050e967e4
Hoamz123 <dvhoa2171@gmail.com>
2024-09-04 13:42:10 +0700

    Update demo2.cpp

--- demo2.cpp
+++ demo2.cpp
@@ -1,8 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
+void budbleSort(int a[],int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-1-i;j++){
+            if(a[j] > a[j+1]){
+                swap(a[j],a[j+1]);
+            }
+        }
+    }
+}
 int main(){
     int n;cin >> n;
-    for(int i=0;i<n;i++){
+    int a[n];
+    for(int &x : a){
+        cin >> x;
+    }
+    budbleSort(a,n);
+    for(int i : a){
         cout << i << " ";
     }
 }
