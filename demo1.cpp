4e315f52ebb4961862d7f4b651f54611be6ba96f
Hoamz123 <dvhoa2171@gmail.com>
2024-09-04 13:37:49 +0700

    Update demo1.cpp
    huhuhu

--- demo1.cpp
+++ demo1.cpp
@@ -2,4 +2,11 @@
 using namespace std;
 int main(){
     cout << "learn git" << endl;
+    vector<int> v;
+    for(int i=0;i<10;i++){
+        v.push_back(i);
+    }
+    for(int i: v){
+        cout << i << " ";
+    }
 }
